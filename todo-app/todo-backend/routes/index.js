dd9bcb10768522791da1f15392b78b7421dcdb1c
Diogo Silva <73388219+clebinho1234@users.noreply.github.com>
2024-07-29 11:24:48 +0100

    Add files via upload

--- todo-app/todo-backend/routes/index.js
+++ todo-app/todo-backend/routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const { getAsync } = require('../redis');
 
 const configs = require('../util/config')
 
@@ -13,6 +14,17 @@ router.get('/', async (req, res) => {
     ...configs,
     visits
   });
+});
+
+/* GET statistics data. */
+router.get('/statistics', async (req, res) => {
+  try {
+    const todoCount = await getAsync('added_todos') || '0';
+    res.status(200).json({ added_todos: parseInt(todoCount, 10) });
+  } catch (err) {
+    console.error('Error fetching statistics:', err);
+    res.status(500).send({ error: 'Internal Server Error' });
+  }
 });
 
 module.exports = router;
